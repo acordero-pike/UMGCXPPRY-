6537595507a9233106aa0a65b9c9da1d9cc986b5
acordero-pike <76223645+acordero-pike@users.noreply.github.com>
2021-09-06 19:25:18 -0600

    Add files via upload

---
+++ cxp.Model/usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cxp.Model
+{
+    public class usuario
+    {
+        public int id_usuario { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public string email { get; set; }
+        public int id_tipo_usuario { get; set; }
+        public string password { get; set; }
+    }
+}
