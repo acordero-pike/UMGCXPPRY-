5ab646591a2f671e4e41312c2c81730755971ad9
ggarciaumg976 <90229212+ggarciaumg976@users.noreply.github.com>
2021-09-22 19:14:54 -0600

    Inicio

--- cxp/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ cxp/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,70 +13,70 @@ namespace cxp.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\albin\Desktop\UMG CXP PRY\UMGCXPPRY-\cxp\_Imports.razor"
+#line 1 "C:\Aplicaciones\UMG cxp PROY\UMGCXPPRY-\cxp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\albin\Desktop\UMG CXP PRY\UMGCXPPRY-\cxp\_Imports.razor"
+#line 2 "C:\Aplicaciones\UMG cxp PROY\UMGCXPPRY-\cxp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\albin\Desktop\UMG CXP PRY\UMGCXPPRY-\cxp\_Imports.razor"
+#line 3 "C:\Aplicaciones\UMG cxp PROY\UMGCXPPRY-\cxp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\albin\Desktop\UMG CXP PRY\UMGCXPPRY-\cxp\_Imports.razor"
+#line 4 "C:\Aplicaciones\UMG cxp PROY\UMGCXPPRY-\cxp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\albin\Desktop\UMG CXP PRY\UMGCXPPRY-\cxp\_Imports.razor"
+#line 5 "C:\Aplicaciones\UMG cxp PROY\UMGCXPPRY-\cxp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\albin\Desktop\UMG CXP PRY\UMGCXPPRY-\cxp\_Imports.razor"
+#line 6 "C:\Aplicaciones\UMG cxp PROY\UMGCXPPRY-\cxp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\albin\Desktop\UMG CXP PRY\UMGCXPPRY-\cxp\_Imports.razor"
+#line 7 "C:\Aplicaciones\UMG cxp PROY\UMGCXPPRY-\cxp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\albin\Desktop\UMG CXP PRY\UMGCXPPRY-\cxp\_Imports.razor"
+#line 8 "C:\Aplicaciones\UMG cxp PROY\UMGCXPPRY-\cxp\_Imports.razor"
 using cxp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\albin\Desktop\UMG CXP PRY\UMGCXPPRY-\cxp\_Imports.razor"
+#line 9 "C:\Aplicaciones\UMG cxp PROY\UMGCXPPRY-\cxp\_Imports.razor"
 using cxp.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\albin\Desktop\UMG CXP PRY\UMGCXPPRY-\cxp\Pages\FetchData.razor"
+#line 3 "C:\Aplicaciones\UMG cxp PROY\UMGCXPPRY-\cxp\Pages\FetchData.razor"
 using cxp.Data;
 
 #line default
@@ -91,7 +91,7 @@ using cxp.Data;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 39 "C:\Users\albin\Desktop\UMG CXP PRY\UMGCXPPRY-\cxp\Pages\FetchData.razor"
+#line 39 "C:\Aplicaciones\UMG cxp PROY\UMGCXPPRY-\cxp\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
